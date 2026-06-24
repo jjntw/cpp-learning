9383e9018c93bcd37530908b635c452f9650f2c5
jjntw <128141774+jjntw@users.noreply.github.com>
2026-06-25 06:03:13 +1000

    sorting functionality

--- stage0/00-tung-tung-sort/main.cpp
+++ stage0/00-tung-tung-sort/main.cpp
@@ -11,9 +11,23 @@
 
 struct Sahur {
     bool operator()(const Vector3D& v1, const Vector3D& v2) const {
-        return v1.norm() < v2.norm();
+        return v1.norm() > v2.norm();
     }
 };
+
+constexpr std::string stringify_vector_vector(std::vector<Vector3D> vec) {
+    return "[" + std::accumulate(vec.begin(), vec.end(),
+                      std::string(), [](std::string s, Vector3D& tung3) {
+                      return s + (s.empty() ? "" : ", ")
+                              + "("
+                              + std::format("{:.2f}, {:.2f}, {:.2f}",
+                                      std::floor(tung3.getX() * 100.0) / 100.0,
+                                      std::floor(tung3.getY() * 100.0) / 100.0,
+                                      std::floor(tung3.getZ() * 100.0) / 100.0
+                              )
+                              + ")";
+    }) + "]";
+}
 
 void tung_tung_sort(const size_t size) {
     using Tung = std::priority_queue<Vector3D, std::vector<Vector3D>, Sahur>;
@@ -22,7 +36,7 @@ void tung_tung_sort(const size_t size) {
     Tung tungtung(Sahur{}, std::move(tung));
     std::vector<Vector3D> sortSpace(size);
 
-    for (size_t i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; ++i) {
         std::string rawLine;
         double t1 = 0.0, t2 = 0.0, t3 = 0.0;
 
@@ -35,7 +49,7 @@ void tung_tung_sort(const size_t size) {
             if (std::cin.fail()) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                std::cout << "Enter valid coordinates (format: <x> <y> <z>).\n" << std::endl;
+                std::cout << "Enter valid coordinates (format: <x> <y> <z>)." << std::endl;
                 continue;
             }
 
@@ -47,27 +61,49 @@ void tung_tung_sort(const size_t size) {
     }
 
     std::cout << "\nYour vector (2.d.p):" << std::endl;
-    std::cout
-    << "["
-    << std::accumulate(sortSpace.begin(), sortSpace.end(),
-            std::string(), [](std::string s, Vector3D& tung3) {
-            return s + (s.empty() ? "" : ", ")
-                     + "("
-                     + std::format("{:.2f}, {:.2f}, {:.2f}",
-                            std::floor((tung3.getX() * 100.0) / 100.0),
-                            std::floor((tung3.getY() * 100.0) / 100.0),
-                            std::floor((tung3.getZ() * 100.0) / 100.0)
-                     )
-                     + ")";
-            })
-    << "]"
-    << std::endl;
+    std::cout << stringify_vector_vector(sortSpace) << std::endl;
 
-    std::cout << "\nSorting Sahur..." << std::endl;
+    std::cout << "\nSorting Sahur...\n" << std::endl;
+
+    Sahur tun;
+    Vector3D gTun, gSahur;
+    for (size_t i = 0; i < size; ++i) {
+        gSahur = tungtung.top();
+        tungtung.pop();
+
+        for (size_t j = i; j < size; ++j) {
+            gTun = sortSpace[j];
+            if (tun(gTun, gSahur)) {
+                continue;
+            }
+
+            std::swap(sortSpace[i], sortSpace[j]);
+            break;
+        }
+
+        std::cout << "Tung..." << std::endl;
+        std::cout << stringify_vector_vector(sortSpace) << '\n' << std::endl;
+    }
 }
 
 int main() {
-    tung_tung_sort(3);
+    std::string rawSize;
+    size_t inputSize = 0;
 
+    std::cout << "Enter the size for your vector." << std::endl;
+    for (;;) {
+        std::cin >> inputSize;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Enter a valid size." << std::endl;
+            continue;
+        }
+        break;
+    }
+
+    tung_tung_sort(inputSize);
+
+    std::cout << "Shaur" << std::endl;
     return 0;
 }
